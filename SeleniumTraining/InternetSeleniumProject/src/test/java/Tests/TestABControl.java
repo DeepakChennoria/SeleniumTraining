e3ffc07114c0477105d62832537c9ed8f62adfa6
DPK <deepak.chennoria@gmail.com>
2020-04-23 21:04:57 +0530

    add

---
+++ SeleniumTraining/InternetSeleniumProject/src/test/java/Tests/TestABControl.java
@@ -0,0 +1,31 @@
+package Tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.pages.ABpage;
+import com.pages.homePage;
+
+import testbase.base;
+
+public class TestABControl extends base {
+    
+	@Test
+	public void testdata()
+	{
+		homePage hp = new homePage(driver);
+		ABpage ap= new ABpage(driver);
+		
+		hp.clickonABtesting();
+		String a = ap.getABtext();
+		Assert.assertEquals(ap.getABtext(),"A/B Test Control");
+		ap.clickonElementalSelenium();
+		
+		
+		 
+		
+	}
+	
+	
+	
+}
---
+++ SeleniumTraining/InternetSeleniumProject/src/test/java/testbase/base.java
@@ -0,0 +1,38 @@
+package testbase;
+/*
+ * prerequisite
+ * browserlaunch
+ * postrequisite
+ * 
+ *  * 
+ * */
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+
+public class base {
+
+	public static WebDriver driver= null;
+	
+		
+	@BeforeTest
+public void launchbrowser()
+{
+		String driverPath=System.getProperty("user.dir")+"\\Resources";
+		System.setProperty("webdriver.chrome.driver",driverPath+"\\chromedriver.exe");
+		 driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://the-internet.herokuapp.com/");
+}
+@AfterTest
+	public void teardown()
+	{
+		driver.quit();
+	}
+	
+}
